7a06ffeafa18c5a75e2e11eed79741dbcdcfc186
Angela Dona <pikena.pzo@hotmail.com>
2022-12-01 18:30:41 -0300

    commit

---
+++ js/curtir.js
@@ -0,0 +1,43 @@
+const dandoLike1 = document.querySelector(".curtida1");
+dandoLike1.addEventListener('click', function() {
+    dandoLike1.classList.toggle('curtido1');
+  })
+  
+
+const dandoLike2 = document.querySelector(".curtida2");
+dandoLike2.addEventListener('click', function() {
+    dandoLike2.classList.toggle('curtido2');
+  })
+  
+
+const dandoLike3 = document.querySelector(".curtida3");
+dandoLike3.addEventListener('click', function() {
+    dandoLike3.classList.toggle('curtido3');
+  })
+  
+
+const dandoLike4 = document.querySelector(".curtida4");
+dandoLike4.addEventListener('click', function() {
+    dandoLike4.classList.toggle('curtido4');
+  })
+  
+
+const dandoLike5 = document.querySelector(".curtida5");
+dandoLike5.addEventListener('click', function() {
+    dandoLike5.classList.toggle('curtido5');
+  })
+  
+const dandoLike6 = document.querySelector(".curtida6");
+dandoLike6.addEventListener('click', function() {
+    dandoLike6.classList.toggle('curtido6');
+  })
+  
+const dandoLike7 = document.querySelector(".curtida7");
+dandoLike7.addEventListener('click', function() {
+    dandoLike7.classList.toggle('curtido7');
+  })
+  
+const dandoLike8 = document.querySelector(".curtida8");
+dandoLike8.addEventListener('click', function() {
+    dandoLike8.classList.toggle('curtido8');
+  })
